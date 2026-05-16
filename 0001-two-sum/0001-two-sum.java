6f16217acf8e8dd7026f4d5ab88fe6aa6abb2319
codes-ops <codesops17@gmail.com>
2026-05-16 11:51:07 +0530

    Time: 34 ms (39.86%), Space: 47.1 MB (58.01%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -22,7 +22,7 @@ class Solution {
 
 
 
-// int left = 0;
+//         int left = 0;
 //         int right = nums.length-1;
 //         while(left<right){
 //             if(nums[left]+nums[right]==target){
