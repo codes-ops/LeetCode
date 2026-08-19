6c82e1b3c5b0a3ed35d56cd3e1db4af4fc0dfe7b
codes-ops <codesops17@gmail.com>
2026-08-19 15:14:06 +0530

    Time: 4 ms (16.53%), Space: 45.6 MB (10.15%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,44 +1,44 @@
-import java.util.*;
 class Solution {
-    public void func(List<Integer>output,List<Integer> input,List<List<Integer>> ans){
+    public void func(List<Integer> input,List<Integer> output,List<List<Integer>> ans){
 
-        ans.add(new ArrayList<>(output));
-
+        ans.add(new ArrayList<>(output)); // adding elements to the power set -->> ans 
         if(input.size()==0){
             return;
         }
         for(int i=0;i<input.size();i++){
+
+            if(i>0 && input.get(i)==input.get(i-1)){
+                continue;
+            }
+
             List<Integer> ip = new ArrayList<>(input);
             List<Integer> op = new ArrayList<>(output);
-            op.add(ip.get(i));
-            ip.subList(0,i+1).clear();
-            func(op,ip,ans);
+
+            op.add(ip.get(i));  // adding elements from input list to the output list
+            ip.subList(0,i+1).clear();  // for deleting elements from the input list
+
+            func(ip,op,ans);
+            
         }
 
     }
     public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
         List<Integer> input = new ArrayList<>();
         List<Integer> output = new ArrayList<>();
         List<List<Integer>> ans = new ArrayList<>();
-        Arrays.sort(nums);
-        for(int a:nums){
+
+        for(int a: nums){
             input.add(a);
-
         }
-        func(output,input,ans);
+        func(input,output,ans);
 
-
-        // this hashset copies only unnique elements
-        HashSet<List<Integer>> set = new HashSet<>(ans);
-
-        List<List<Integer>> result = new ArrayList<>(set);
-
-        return result;
+        return ans;
     }
 }
 
-// DO NOT CALCULATE THE POWER SET THE PROBLEM IS ONLY ASKING FOR UNIQUE COMBINATIONS
-// result arraylist only contains distinct elements
+
+
 
 
 
