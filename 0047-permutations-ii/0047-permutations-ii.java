85f71525938727a1d3166de65c701fe4a94f1c09
codes-ops <codesops17@gmail.com>
2026-08-21 10:27:59 +0530

    Time: 5 ms (18.12%), Space: 46.4 MB (94.9%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -0,0 +1,41 @@
+class Solution {
+    public void func(List<Integer> input,List<Integer> output,List<List<Integer>> ans){
+
+        if(input.size()==0){
+            ans.add(new ArrayList<>(output));
+            return;
+        }
+
+        for(int i=0;i<input.size();i++){
+
+            if(i>0 && input.get(i)==input.get(i-1)){
+                continue;
+            }
+
+            List<Integer> ip = new ArrayList<>(input);
+            List<Integer> op = new ArrayList<>(output);
+            
+            op.add(ip.get(i));  
+            ip.remove(i);  
+
+            func(ip,op,ans);
+
+        }
+
+    }
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        Arrays.sort(nums);
+        List<Integer> input = new ArrayList<>();
+        List<Integer> output = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int a: nums){
+            input.add(a);
+        }
+        func(input,output,ans);
+
+        return ans;
+        
+    }
+}
+
+
