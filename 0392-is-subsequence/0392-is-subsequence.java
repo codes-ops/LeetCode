b296fffdc1cfb5df61ebe510daccf4cff45411a3
codes-ops <codesops17@gmail.com>
2026-08-22 19:46:34 +0530

    Time: 1 ms (95.68%), Space: 43.7 MB (5.2%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean func(int i,int j,String s, String t){
+
+        if(i>=s.length()){
+            return true;
+        }
+        if(j>=t.length()){
+            return false;
+        }
+
+        if(s.charAt(i)==t.charAt(j)){
+            return func(i+1,j+1,s,t);
+            
+        }
+        else{
+            return func(i,j+1,s,t);
+            
+        }
+        
+
+
+    }
+    public boolean isSubsequence(String s, String t) {
+        return func(0,0,s,t);
+        
+    }
+}
+
+
+
