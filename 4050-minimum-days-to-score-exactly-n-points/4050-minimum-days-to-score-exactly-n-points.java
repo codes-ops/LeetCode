feba758d167a8a91e098b1c1e96f4442fb163130
codes-ops <codesops17@gmail.com>
2026-09-18 09:09:28 +0530

    Time: 1453 ms (5%), Space: 286.7 MB (5.15%) - LeetHub

---
+++ 4050-minimum-days-to-score-exactly-n-points/4050-minimum-days-to-score-exactly-n-points.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int func(int n,int score,int streak,int[][] dp){
+        if(score==n){
+            return 0;
+        }
+        if(score>n){
+            return 1000000000;
+        }
+        if(dp[score][streak]!=-1){
+            return dp[score][streak];
+        }
+        int a = 1 + func(n,score+streak+1,streak+1,dp);
+        int b = 1000000000;
+        if(streak>0){
+            b = 1 + func(n,score,0,dp);
+        }
+        return dp[score][streak]=Math.min(a,b);
+    }
+    public int minDays(int n) {
+        int[][] dp  = new int[n][460];
+        for(int i=0;i<n;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        int ans = func(n,0,0,dp);
+        return ans;
+    }
+}
+
+
+
+
+
